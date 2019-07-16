564d5b0372ca9bd0c5194a364f2fc47af29455b2
jaybenaim <benaimjacob@gmail.com>
2019-07-16 14:45:30 -0400

    added exercise to import_practice.py

---
+++ import_practice.py
@@ -0,0 +1,20 @@
+import ascii 
+
+print(ascii.one()) 
+print(ascii.two()) 
+
+from art import pictures 
+
+print(pictures.three())
+print(pictures.four())
+
+from art import even_more_art 
+from art.even_more_art import images 
+
+print(images.five())
+
+
+
+import scene as s 
+
+print(s.six())
